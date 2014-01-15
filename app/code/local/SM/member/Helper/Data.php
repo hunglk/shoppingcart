d24b52792a468499a24c0abfed874122bfab1280
unknown <khachung87hn@gmail.com>
2014-01-15 13:58:31 +0700

    hunglk

---
+++ app/code/local/SM/member/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HungLK
+ * Date: 1/13/14
+ * Time: 5:36 PM
+ */ 
+class SM_member_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/SM/member/sql/sm_member_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HungLK
+ * Date: 1/13/14
+ * Time: 5:36 PM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
